0773e05b73871b45dd91dac4e632eb7bd7c91bae
Lamm-nguy <112396747+Lamm-nguy@users.noreply.github.com>
2022-09-11 15:45:14 +0700

    Add files via upload

---
+++ Concept AI.py
@@ -0,0 +1,39 @@
+print(               --------------------#Welcome to F-AI!#----------------------            )
+print(Type f-quest-list to get question list)
+print(Type f-work to get some of question)
+
+if you == " " 
+   AI = " Try Again "
+elif you == " hello "
+   AI = " hi "
+elif you == "how are you ?"
+   AI = " i'm fine, thank you "
+elif you == "how are you"
+   AI = " i'm fine, thank you "
+ elif you == "search"
+   AI = "Google.com"
+ elif you == "today is "
+   AI = " it's Sunday, 11st September 2022 "
+ elif you == "translate" 
+   AI = "you can use it in Google by i can't do that"
+ elif you == "what is BCI ?"
+   AI = "it's Brain-Computer-Interface"
+ elif you == " how old are you "
+  AI = " now i'm 0 years old"
+ elif you == " what's your favorite food ?"
+  AI = "i don't have favorite food "
+ elif you == " i love you"
+  AI = "sorry, i don't love you"
+
+ elif you == "f-quest-list"
+  print(hello)
+  print(how are you)
+  print(search)
+  print(today is)
+  print(translate)
+  print(what is BCI ?)
+  print(how old are you ?)
+
+ 
+ print(AI)
+ 
